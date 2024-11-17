8a6ae46228122113b3a20730c9319a19454cc96e
FelixH3r <felix.hermanutz@gmx.at>
2024-11-17 20:42:58 +0100

    quick fix select component

--- components/utils/SelectComponent.vue
+++ components/utils/SelectComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative">
     <select
-        class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+        class="py-3 px-4 pe-9 block w-full border-gray-200 bg-white rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
         v-model="selected" @change="handleChange"
     >
       <option v-for="(value, index) in values"> {{ value }}</option>
